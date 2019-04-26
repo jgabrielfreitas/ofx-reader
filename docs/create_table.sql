ef8c10351a387fd35d989d9d97285ba4a34031d7
damiao <damiao.junior@outlook.com>
2019-04-26 19:50:41 -0300

    :memo: Update transaction table script

--- docs/create_table.sql
+++ docs/create_table.sql
@@ -1,22 +1,20 @@
 create table [transaction]
 (
-  id bigint identity,
-  bank_id int not null,
-  account_id nvarchar(24) not null,
-  transaction_id bigint not null,
-  transaction_date datetime2 not null,
-  transaction_type nvarchar(24) not null,
-  transaction_description nvarchar(1024),
-  file_id nvarchar(1024) not null
+    id                      bigint identity
+        constraint transaction_pk
+            primary key nonclustered,
+    bank_id                 int            not null,
+    account_id              nvarchar(24)   not null,
+    transaction_id          bigint         not null,
+    transaction_date        datetime2      not null,
+    transaction_type        nvarchar(24)   not null,
+    transaction_description nvarchar(1024),
+    file_id                 nvarchar(1024) not null,
+    transaction_id_hash     nvarchar(512)  not null
 )
 go
 
 create unique index transaction_id_uindex
-  on [transaction] (id)
-go
-
-alter table [transaction]
-  add constraint transaction_pk
-    primary key nonclustered (id)
+    on [transaction] (id)
 go
 
