c0f27eae64ec72706fe56876f42ca6dcb69bec74
damiao <damiao.junior@outlook.com>
2019-04-26 13:44:59 -0300

    :bug: Update file directory configuration object

--- src/OFX.Reader.Infrastructure/FileManager/OFXDirectorySettings.cs
+++ src/OFX.Reader.Infrastructure/FileManager/OFXDirectorySettings.cs
@@ -2,8 +2,8 @@ namespace OFX.Reader.Infrastructure.FileManager {
 
     public class OFXDirectorySettings {
 
-        public string OFXFileDirectory { get; } = @"..\..\..\..\..\ofx_files";
-        public string OFXProcessedFileDirectory { get; } = @"..\..\..\..\..\ofx_processed_files";
+        public string OFXFileDirectory { get; set; } = @"..\..\ofx_files";
+        public string OFXProcessedFileDirectory { get; set; } = @"..\..\ofx_processed_files";
 
     }
 
